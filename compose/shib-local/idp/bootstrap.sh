9853aa22688f90cddf8109ab30df5d21e111c536
Richard Lowe <lower29@users.noreply.github.com>
2017-09-11 10:23:50 +0100

    Compose: fix JRE path in idp image to avoid breaks from zulu version changes (#64)
    The /opt/jre-home symlink is already present in the base image, so just reusing that.
    
    This fixes #61.

--- compose/shib-local/idp/bootstrap.sh
+++ compose/shib-local/idp/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ZULU_HOME=/opt/zulu8.20.0.5-jdk8.0.121-linux_x64
+JRE_HOME=/opt/jre-home
 
 if [ -s /usr/share/pki/ca-trust-source/anchors/${DOMAIN_NAME}-ca.crt ] ; then
 	exit
@@ -9,9 +9,9 @@ fi
 # Copy CA cert into trusted location and update Linux trusted certs registry
 cp -p /secrets/${DOMAIN_NAME}-ca.crt /usr/share/pki/ca-trust-source/anchors/ && update-ca-trust
 # Also update JRE trusted certs so Tomcat trusts it too
-chmod +x ${ZULU_HOME}/bin/keytool
+chmod +x ${JRE_HOME}/bin/keytool
 sleep 5
-${ZULU_HOME}/bin/keytool -import -noprompt -trustcacerts -alias ${DOMAIN_NAME} \
+${JRE_HOME}/bin/keytool -import -noprompt -trustcacerts -alias ${DOMAIN_NAME} \
 	-file /secrets/${DOMAIN_NAME}-ca.crt \
-	-keystore  /opt/zulu8.20.0.5-jdk8.0.121-linux_x64/jre/lib/security/cacerts \
+	-keystore  ${JRE_HOME}/lib/security/cacerts \
 	-storepass changeit
